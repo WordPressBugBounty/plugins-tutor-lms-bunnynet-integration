e0bb5c7519c333bc20364127826e982ead5f66ab
Sean Murphy <sean@iamseanmurphy.com>
2026-07-10 07:51:21 +0000

    1.0.2

--- tutor-lms-bunnynet-integration/vendor/composer/installed.php
+++ tutor-lms-bunnynet-integration/vendor/composer/installed.php
@@ -1,9 +1,9 @@
 <?php return array(
     'root' => array(
         'name' => 'tutor/bunny-net-integration',
-        'pretty_version' => 'v1.0.1',
-        'version' => '1.0.1.0',
-        'reference' => 'eb03471c9e2116162f36fb1ad76cd4cc425c9dc0',
+        'pretty_version' => 'v1.0.2',
+        'version' => '1.0.2.0',
+        'reference' => 'c593778ba72fe7691b69230e843607942db6ce10',
         'type' => 'library',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -11,9 +11,9 @@
     ),
     'versions' => array(
         'tutor/bunny-net-integration' => array(
-            'pretty_version' => 'v1.0.1',
-            'version' => '1.0.1.0',
-            'reference' => 'eb03471c9e2116162f36fb1ad76cd4cc425c9dc0',
+            'pretty_version' => 'v1.0.2',
+            'version' => '1.0.2.0',
+            'reference' => 'c593778ba72fe7691b69230e843607942db6ce10',
             'type' => 'library',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
